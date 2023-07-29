95d5e07eb45711af4ca9417ff9ea669c925b7508
Abi-097 <abimahen0312@gmail.com>
2023-07-29 11:52:00 +0530

    created pages and routed

---
+++ portfolio/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Experience from './pages/Experience';
+
+function App() {
+  return (
+    <div className="App">
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/experience' element={<Experience/>}/>
+      </Routes>
+    </Router>
+    </div>
+  );
+}
+
+export default App;

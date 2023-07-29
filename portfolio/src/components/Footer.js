6beeee54a0e0848a87af288f95183334fc6fb717
Abi-097 <abimahen0312@gmail.com>
2023-07-29 16:18:34 +0530

    home page done

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,5 +1,4 @@
 import React from "react";
-import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import EmailIcon from "@mui/icons-material/Email";
@@ -12,7 +11,6 @@ export default function Footer() {
         <GitHubIcon />
         <LinkedInIcon />
         <EmailIcon />
-        <WhatsAppIcon />
         <FacebookIcon />
       </div>
 

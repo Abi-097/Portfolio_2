a05bbd72ea7c9cb2d2bfdd991782bc80cfa1c8ed
Abi-097 <120549516+Abi-097@users.noreply.github.com>
2024-06-25 22:34:26 +0530

    data added

--- portfolio/app/components/Skills.js
+++ portfolio/app/components/Skills.js
@@ -120,10 +120,12 @@ const Skills = () => {
     <Container id="skills">
       <Wrapper>
         <Title>Skills</Title>
+
         <Desc>
           Here are some of my skills on which I have been working on for the
           past 2 years.
         </Desc>
+
         <SkillsContainer>
           {skills.map((skill, index) => (
             <Skill key={index}>
